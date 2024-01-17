c83198f387d241658cd1d0766f67c0830f0e2ab8
moonsoo364 <aptx15hibr@naver.com>
2024-01-17 12:59:48 +0900

    매트릭스, 부등호 문제 패키지 생성

---
+++ src/InequlitySign/InEquiltySign2529.java
@@ -0,0 +1,5 @@
+package InequlitySign;
+
+public class InEquiltySign2529 {
+
+}
---
+++ src/matrix/matrix1080.java
@@ -0,0 +1,21 @@
+package matrix;
+
+import java.util.Scanner;
+
+public class matrix1080 {
+	
+	static Scanner stdIn = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("N: ");
+		int n = stdIn.nextInt();
+		int[] nAry = new int[n];
+		
+		System.out.println("M: ");
+		int m = stdIn.nextInt();
+		int[] mAry = new int[m];
+		
+		
+	}
+
+}
