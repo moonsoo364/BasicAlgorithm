363d2352416932375f0cd59a242dbb00e3ea6beb
moonsoo364 <aptx15hibr@naver.com>
2024-01-18 17:05:24 +0900

    반복문 더하기 추가

---
+++ src/loop/Plus10951.java
@@ -0,0 +1,30 @@
+package loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Plus10951 {
+	
+	public static void main(String[] args) {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true){
+			
+			String line;
+			try {
+				line = bf.readLine();
+				if(line == null || line.equals(""))
+					break;
+				String[] split = line.split(" ");
+				int a = Integer.parseInt(split[0]);
+				int b = Integer.parseInt(split[1]);
+				System.out.println(a+b);
+			} catch (IOException e) {
+				break;
+			}
+
+		}
+	}
+
+}
